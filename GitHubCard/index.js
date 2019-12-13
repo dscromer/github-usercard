dfd149571b05b7b4d990e5ff1f517ba86151bf9c
unknown <cromer.dalton@gmail.com>
2019-12-12 19:51:28 -0500

    project complete with no stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+  .get('https://api.github.com/users/dscromer')
+  .then((res) => {
+    console.log(res);
+    const cardSection = document.querySelector('.cards');
+    cardSection.appendChild(newCard(res));
+  })
+  .catch((err) => {
+    console.log(`You hit an error: ${err}`);
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +34,19 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach((user) => {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then((res) => {
+      const cardSection = document.querySelector('.cards');
+      cardSection.appendChild(newCard(res));
+    })
+    .catch((err) => {
+      console.log(`You hit an error: ${err}`);
+    });
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -46,6 +68,46 @@ const followersArray = [];
 
 */
 
+function newCard(user) {
+  const card = document.createElement('div');
+  const cardImage = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardName = document.createElement('h3');
+  const cardUser = document.createElement('p');
+  const cardLoc = document.createElement('p');
+  const cardProf = document.createElement('p');
+  const cardFollower = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  cardName.classList.add('name');
+  cardUser.classList.add('username');
+
+  cardImage.src = user.data.avatar_url;
+  cardName.textContent = user.data.name;
+  cardUser.textContent = user.data.login;
+  cardLoc.textContent = `Location: ${user.data.location}`;
+  cardProf.textContent = `Profile: ${user.data.html_url}`;
+  cardFollower.textContent = `Followers: ${user.data.followers}`;
+  cardFollowing.textContent = `Following: ${user.data.following}`;
+  cardBio.textContent = `Bio: ${user.data.bio}`;
+
+  card.appendChild(cardImage);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(cardUser);
+  cardInfo.appendChild(cardLoc);
+  cardInfo.appendChild(cardProf);
+  cardInfo.appendChild(cardFollower);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
+  
+
+  return card;
+}
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -53,3 +115,36 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+// data:
+// avatar_url: "https://avatars0.githubusercontent.com/u/55195593?v=4"
+// bio: "Full Stack Web Dev student at Lambda School"
+// blog: ""
+// company: null
+// created_at: "2019-09-11T13:32:05Z"
+// email: null
+// events_url: "https://api.github.com/users/dscromer/events{/privacy}"
+// followers: 5
+// followers_url: "https://api.github.com/users/dscromer/followers"
+// following: 5
+// following_url: "https://api.github.com/users/dscromer/following{/other_user}"
+// gists_url: "https://api.github.com/users/dscromer/gists{/gist_id}"
+// gravatar_id: ""
+// hireable: null
+// html_url: "https://github.com/dscromer"
+// id: 55195593
+// location: null
+// login: "dscromer"
+// name: "Dalton Cromer"
+// node_id: "MDQ6VXNlcjU1MTk1NTkz"
+// organizations_url: "https://api.github.com/users/dscromer/orgs"
+// public_gists: 0
+// public_repos: 21
+// received_events_url: "https://api.github.com/users/dscromer/received_events"
+// repos_url: "https://api.github.com/users/dscromer/repos"
+// site_admin: false
+// starred_url: "https://api.github.com/users/dscromer/starred{/owner}{/repo}"
+// subscriptions_url: "https://api.github.com/users/dscromer/subscriptions"
+// type: "User"
+// updated_at: "2019-12-13T00:26:45Z"
+// url: "https://api.github.com/users/dscromer"
